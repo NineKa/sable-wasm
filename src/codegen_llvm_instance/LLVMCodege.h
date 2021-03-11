59fc3cd4107516a7c98606653a26bd15d5773596
NineKA <NineKa@users.noreply.github.com>
2021-03-10 23:24:42 -0500

    llvm codegen

---
+++ src/codegen_llvm_instance/LLVMCodege.h
@@ -0,0 +1,9 @@
+#ifndef SABLE_INCLUDE_GUARD_CODEGEN_LLVM_CTX_LLVM_CODEGEN
+#define SABLE_INCLUDE_GUARD_CODEGEN_LLVM_CTX_LLVM_CODEGEN
+
+#include "../mir/Instruction.h"
+#include "../mir/Module.h"
+
+namespace codegen::llvm_instance {}
+
+#endif
--- src/mir/LLVMCodegen.cc
+++
@@ -1,6 +0,0 @@
-#include "LLVMCodegen.h"
-
-namespace mir::llvm_codegen {
-class TranslationTask::TranslationContext {};
-
-} // namespace mir::llvm_codegen

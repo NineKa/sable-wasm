f16f71f937e14b42f15626e06fdb3cbe38af82a0
NineKA <NineKa@users.noreply.github.com>
2021-04-21 01:41:52 -0400

    plot as speedup

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 import numpy as np
 
 
@@ -61,19 +62,19 @@ def parse_data(path):
     }
 
 
-def relative_plt(file_name, x_label, d0, d1, label0 = 'd0') :
+def relative_plt(file_name, x_label, d0, d1, label1 = 'd0') :
     x = np.arange(len(x_label))
     width = 0.618
 
     fig, ax = plt.subplots()
     axes = plt.gca()
-    axes.set_ylim([0, 2])
+    #axes.set_ylim([0, 2])
 
     d0 = np.array(d0)
     d1 = np.array(d1)
-    d0 = d0 / d1
+    d1 = d0/d1
 
-    pl0 = ax.bar(x, d0, width, label = label0)
+    pl1 = ax.bar(x, d1, width, label=label1)
     ax.axhline(y = 1, linestyle = '--')
 
     ax.set_xticks(x)
@@ -81,29 +82,82 @@ def relative_plt(file_name, x_label, d0, d1, label0 = 'd0') :
     ax.legend()
 
     fig.tight_layout()
+    plt.grid(True)
     plt.savefig(file_name)
     plt.close(fig)
+
+def sablewasm_plt(file_name, x_label, d0, d1, d2, label1 = 'd1', label2 = 'd2'):
+    x = np.arange(len(x_label))
+    width = 0.4
+
+    fig, ax = plt.subplots()
+    axes = plt.gca()
+    #axes.set_ylim([0, 1.2])
+
+    d0 = np.array(d0)
+    d1 = np.array(d1)
+    d2 = np.array(d2)
+    d1 = d0/d1
+    d2 = d0/d2
+
+    pl1 = ax.bar(x - width / 2, d1, width, label = label1)
+    pl2 = ax.bar(x + width / 2, d2, width, label = label2)
+    ax.axhline(y = 1, linestyle = '--')
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_label, rotation='vertical')
+    ax.legend()
+
+    fig.tight_layout()
+    plt.grid(True)
+    plt.savefig(file_name)
+    plt.close(fig)
+
 
 polybench = parse_data('SableWasm Benchmark - polybench-c-4.2.1-beta.csv')
 for mode in ['naive', 'opt', 'simd']:
     for toolchain in ['wasmtime', 'wasmer-cranelift', 'wasmer-llvm']:
         relative_plt('plots/polybench-relative-{}-{}.pdf'.format(toolchain, mode),
-             polybench['target'], polybench['sablewasm_{}'.format(mode)],
+             polybench['target'], 
              polybench['{}_{}'.format(toolchain, mode)],
+             polybench['sablewasm_{}'.format(mode)],
              'sablewasm_{}'.format(mode))
+
+sablewasm_plt('plots/polybench.pdf', 
+    polybench['target'],
+    polybench['sablewasm_naive'], 
+    polybench['sablewasm_opt'],
+    polybench['sablewasm_simd'],
+    'sablewasm_opt', 'sablewasm_simd')
 
 npb = parse_data('SableWasm Benchmark - NPB3.0-omp-C.csv')
 for mode in ['naive', 'opt', 'simd']:
     for toolchain in ['wasmtime', 'wasmer-cranelift', 'wasmer-llvm']:
         relative_plt('plots/npb-relative-{}-{}.pdf'.format(toolchain, mode),
-             npb['target'], npb['sablewasm_{}'.format(mode)],
+             npb['target'], 
              npb['{}_{}'.format(toolchain, mode)],
+             npb['sablewasm_{}'.format(mode)],
              'sablewasm_{}'.format(mode))
+
+sablewasm_plt('plots/npb.pdf', 
+    npb['target'],
+    npb['sablewasm_naive'], 
+    npb['sablewasm_opt'],
+    npb['sablewasm_simd'],
+    'sablewasm_opt', 'sablewasm_simd')
 
 ostrich = parse_data('SableWasm Benchmark - ostrich.csv')
 for mode in ['naive', 'opt', 'simd']:
     for toolchain in ['wasmtime', 'wasmer-cranelift', 'wasmer-llvm']:
         relative_plt('plots/ostrich-relative-{}-{}.pdf'.format(toolchain, mode),
-             ostrich['target'], ostrich['sablewasm_{}'.format(mode)],
+             ostrich['target'],
              ostrich['{}_{}'.format(toolchain, mode)],
+             ostrich['sablewasm_{}'.format(mode)],
              'sablewasm_{}'.format(mode))
+
+sablewasm_plt('plots/ostrich.pdf', 
+    ostrich['target'],
+    ostrich['sablewasm_naive'], 
+    ostrich['sablewasm_opt'],
+    ostrich['sablewasm_simd'],
+    'sablewasm_opt', 'sablewasm_simd')

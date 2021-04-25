dda1e517595a995dcbc315b192a0fdec362c1311
NineKA <nineka@tuta.io>
2021-04-25 13:28:37 -0700

    run script for npb

---
+++ benchmark/NPB3.0-omp-C/sablewasm.run.py
@@ -0,0 +1,69 @@
+import subprocess
+import os
+import re
+
+terminal_codec = 'utf-8'
+
+targets = [
+    'BT.A',
+    'CG.A',
+    'EP.A',
+    'FT.A',
+    'IS.A',
+    'LU.W',
+    'SP.A',
+]
+
+
+def run(target_name):
+    source = target_name
+    object_file = target_name + '.o'
+    sable_file = target_name + '.sable'
+
+    compile_process = subprocess.run([
+        '../../build/sable-wasm', '--unsafe', '--opt',
+        source,
+        '-o', object_file],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL)
+    if compile_process.returncode != 0:
+        return '#N/A'
+
+    link_proces = subprocess.run(
+        ['ld', '-shared', object_file, '-o', sable_file],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL)
+    if link_proces.returncode != 0:
+        os.remove(object_file)
+        return '#N/A'
+
+    tester_process = subprocess.Popen([
+        '../../build/tester', sable_file],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc_out, proc_error = tester_process.communicate()
+
+    if len(proc_error) != 0:
+        os.remove(object_file)
+        os.remove(sable_file)
+        return '#N/A'
+
+    os.remove(object_file)
+    os.remove(sable_file)
+
+    try:
+        out = proc_out.decode(terminal_codec).replace('\n', '')
+        regex = r'Time in seconds = *([0-9]*.[0-9]*)'
+        matches = re.search(regex, out)
+        if matches is None:
+            return '#N/A'
+        return matches.group(1)
+    except UnicodeDecodeError:
+        return '#N/A'
+
+
+for target in targets:
+    naive_result = run(target + '.naive.wasm')
+    opt_result = run(target + '.opt.wasm')
+    simd_result = run(target + '.simd.wasm')
+
+    print('{}, {}, {}, {}'.format(target, naive_result, opt_result, simd_result))
---
+++ benchmark/NPB3.0-omp-C/wasmer-llvm.run.py
@@ -0,0 +1,55 @@
+import subprocess
+import re
+
+targets = [
+    'BT.A',
+    'CG.A',
+    'EP.A',
+    'FT.A',
+    'IS.A',
+    'LU.W',
+    'SP.A',
+]
+
+
+terminal_codec = 'utf-8'
+
+
+def get_time(out_bytes):
+    try:
+        out = out_bytes.decode(terminal_codec).replace('\n', '')
+        regex = r'Time in seconds = *([0-9]*.[0-9]*)'
+        matches = re.search(regex, out)
+        if matches is None:
+            return '#N/A'
+        return matches.group(1)
+    except UnicodeDecodeError:
+        return "#N/A"
+
+
+def run(target):
+    wasm_module_naive = '{}.naive.wasm'.format(target)
+    wasm_module_opt = '{}.opt.wasm'.format(target)
+    wasm_module_simd = '{}.simd.wasm'.format(target)
+    command_naive = ['wasmer', 'run', '--llvm', wasm_module_naive]
+    command_opt = ['wasmer', 'run', '--llvm', wasm_module_opt]
+    command_simd = ['wasmer', 'run', '--llvm',
+                    '--enable-simd', wasm_module_simd]
+    proc = subprocess.Popen(
+        command_naive, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc_out, proc_error = proc.communicate()
+    time_naive = get_time(proc_out)
+    proc = subprocess.Popen(
+        command_opt, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc_out, proc_error = proc.communicate()
+    time_opt = get_time(proc_out)
+    proc = subprocess.Popen(
+        command_simd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc_out, proc_error = proc.communicate()
+    time_simd = get_time(proc_out)
+    print("{}, {}, {}, {}".format(target, time_naive, time_opt, time_simd))
+
+
+if __name__ == '__main__':
+    for target in targets:
+        run(target)
